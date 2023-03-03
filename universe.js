3d4f99ae92aeb541eae652ee5ea19892313899db
Masud <ranamasud0701@gmail.com>
2023-03-03 17:23:34 +0600

    validation add

--- universe.js
+++ universe.js
@@ -13,11 +13,12 @@ const showData = (data) =>{
     // console.log(data)
      const cardContainer = document.getElementById('card-container');
       const remaining = data.slice(6,12);
+      loading(true);
     
      
      
       const showAll = document.getElementById('show-all');
-         loading(true);
+         
       
       if( data.length>6){
          
@@ -45,12 +46,17 @@ const showData = (data) =>{
       }
 
      
-     
-     
-
      data.forEach(element => {
+
+      const ss = [];
+      document.getElementById('btn-date').addEventListener('click',function(){
+          ss.push(element.published_in);
+            console.log(ss);
+            
+      })
        // console.log(element)
-        
+      
+       
         const div = document.createElement('div');
        
         div.classList.add('col');
@@ -112,6 +118,22 @@ const modalDemo=(data)=>{
 }
 const  showModel =(data)=>{
     console.log(data)
+   let integration = data.integrations;
+   if(integration === null){
+    integration = [];
+   }
+   let price = data.pricing;
+   //console.log(price);
+   if(price == null){
+       price = 'not found';
+   }
+   let input = data.input_output_examples;
+    if(input==null){
+       input = 'not found';
+    }
+   
+  
+   //console.log(integration);
      const modal = document.getElementById('modal-id');
      const div = document.createElement('div');
      modal.innerHTML=" ";
@@ -125,27 +147,26 @@ const  showModel =(data)=>{
         
          <p class="card-text">${data.description}</p>
           <div class="">
-          <button type="button" class="btn  btn-outline-primary disabled mb-2">${data.pricing[0].price ? data.pricing[0].price:'Free of cost' } <br> ${data.pricing[0].plan}</button>
-          <button type="button" class="btn btn-outline-success mb-2 disabled">${data.pricing[1].price?data.pricing[1].price:'Free of cost'} <br> ${data.pricing[1].plan}</button>
-          <button type="button" class="btn btn-outline-danger disabled">${data.pricing[2].price ? data.pricing[2].price: 'Free of cost'
-          } <br> ${data.pricing[2].plan}</button>
+          <button type="button" class="btn btn-outline-primary disabled mb-2">${price ? price[0].price:'Free of cost' } <br> ${price ? price[0].plan:'not found' }</button>
+          <button type="button" class="btn btn-outline-success mb-2 disabled">${price ? price[1].price:'Free of cost'} <br> ${price ? price[1].plan :'not null'}</button>
+          <button type="button" class="btn btn-outline-danger disabled">${price ? price[2].price:'Free of cost'} <br> ${price ? price[2].plan :'not null'}</button>
            </div>
 
            <div>
                <h4>Features</h4>
                <ul>
-               <li>${data.features[1].feature_name === undefined ? 'd-none':data.features[1].feature_name}</li>
-               <li>${data.features[2].feature_name}</li>
-               <li>${data.features[3].feature_name}</li>
+               <li>${data.features[1].feature_name === undefined ? 'not found':data.features[1].feature_name}</li>
+               <li>${data.features[2].feature_name === undefined ? 'not found':data.features[2].feature_name}</li>
+               <li>${data.features[3].feature_name === undefined ? 'not found':data.features[3].feature_name}</li>
              </ul>
 
            </div>
            <div>
                <h4>Integrations</h4>
                <ul>
-               <li>${data.integrations[0]}</li>
-               <li>${data.integrations[1]}</li>
-               <li>${data.integrations[2]}</li>
+               <li>${integration[0] ? integration[0] : 'not found'}</li>
+               <li>${integration[1] ? integration[1] : 'not found'}</li>
+               <li>${integration[2] ? integration[2] : 'not found'}</li>
              </ul>
 
            </div>
@@ -161,8 +182,8 @@ const  showModel =(data)=>{
     
      <img src=${data.image_link[0]} class="card-img-top" alt="...">
        <div class="card-body">
-         <h5 class="card-title">${data.input_output_examples[0].input}</h5>
-         <p class="card-text">${data.input_output_examples[0].output}</p>
+         <h5 class="card-title">${input ? input[0].input :'not found'}</h5>
+         <p class="card-text">${input ? input[0].output :'not found'}</p>
        </div>
      </div>
    </div>           
@@ -183,5 +204,10 @@ const  showModel =(data)=>{
   }
 
 }
+
+//const dateSort=()=>{
+   
+    
+//}
 
 
